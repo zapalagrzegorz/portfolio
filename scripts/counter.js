e99a31402f00cfad4386b595594d0602d515a293
Wanda Kalinowska <wanda0501@gmail.com>
2018-11-17 18:04:10 +0100

    new version

---
+++ scripts/counter.js
@@ -0,0 +1,34 @@
+$(function(){
+
+    /* Counter */
+
+	var aboutTop = $('#counter_int').offset().top - window.innerHeight,
+	counter = $('.counter');
+
+
+		$(window).scroll(function() {
+		
+			$('.counter').each(function() {
+ 				var $this = $(this),
+      			countTo = $this.attr('data-count');
+  
+  			$({ countNum: $this.text()}).animate({
+    			countNum: countTo
+  				},
+
+
+  				{
+    				duration: 3000,
+    				easing:'linear',
+    					step: function() {
+      					$this.text(Math.floor(this.countNum));
+    				},
+    				complete: function() {
+     			 		$this.text(this.countNum);
+      				//alert('finished');
+    			}
+
+  			});  
+		});
+	});
+});
---
+++ scripts/scroll_reveal.js
@@ -0,0 +1,12 @@
+/*pojawianie sie i znikanie elementow strony*/
+window.sr = ScrollReveal();
+		sr.reveal('.icon, .icon_contact', {
+			reset: true,
+			delay: 300,
+			distance: 0
+		});
+		sr.reveal('h1, h2, h3, p ', {
+			reset: true,
+			delay: 300
+		});	
+	
