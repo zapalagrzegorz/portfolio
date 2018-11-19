58ca37eb9ee6691f50e726015844c480449fbdbd
Wanda Kalinowska <wanda0501@gmail.com>
2018-11-19 15:59:28 +0100

    Add smooth scroll menu

---
+++ scripts/smooth_scroll_menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+				$('a[href^="#"]').on('click', function (e) {
+					e.preventDefault();
+
+					var target = this.hash;
+					var $target = $(target);
+
+					$('html, body').animate({
+						'scrollTop': $target.offset().top
+					}, 1000, 'swing');
+				});
+			});
